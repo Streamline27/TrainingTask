21ba347186d974abac6cc27086b9e9770af82853
Vladislav <vladislav.ykshn@gmail.com>
2021-01-20 18:51:25 +0200

    Training task for couning passports

---
+++ src/main/java/test/Main.java
@@ -0,0 +1,40 @@
+package test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static final Function<String, String> REPLACE_NEW_LINE_WITH_SPACE =  it -> it.replace("\n", " ");
+
+    public static final String DATA_TO_RUN = DataSets.COMPLETE_DATASET;
+
+    public static void main(String[] args) {
+        List<Passport> passports = Stream.of(DATA_TO_RUN.split("\n\n"))
+                .map(REPLACE_NEW_LINE_WITH_SPACE)
+                .map(Main::parseString)
+                .map(Passport::fromMap)
+                .filter(Passport::isValidPassport)
+                .collect(Collectors.toList());
+
+        System.out.println("Number of valid passports: " + passports.size());
+    }
+
+    public static Map<String, String> parseString(String rawString) {
+        return Stream.of(rawString.split(" "))
+                .collect(Collectors.toMap(
+                        Main::getKey,
+                        Main::getValue));
+    }
+
+    public static String getKey(String rawKeyValue) {
+        return rawKeyValue.split(":")[0];
+    }
+
+    public static String getValue(String rawKeyValue) {
+        return rawKeyValue.split(":")[1];
+    }
+}
---
+++ src/main/java/test/Passport.java
@@ -0,0 +1,133 @@
+package test;
+
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+@Value
+@Builder
+public class Passport {
+
+    private final static Pattern COLOR_REGEX = Pattern.compile("^[a-f0-9]{6}$");
+    private final static Set<String> ALLOWED_EYE_COLORS = Set.of("amb", "blu", "brn", "gry", "grn", "hzl", "oth");
+
+    String birthYear;
+    String issueYear;
+    String expirationYear;
+    String height;
+    String hairColor;
+    String eyeColor;
+    String passportID;
+    String countryID;
+
+    public static Passport fromMap(Map<String, String> map) {
+        return Passport.builder()
+                .birthYear(map.get("byr"))
+                .issueYear(map.get("iyr"))
+                .expirationYear(map.get("eyr"))
+                .height(map.get("hgt"))
+                .hairColor(map.get("hcl"))
+                .eyeColor(map.get("ecl"))
+                .passportID(map.get("pid"))
+                .countryID(map.get("cid"))
+                .build();
+    }
+
+    public boolean containsAllFields() {
+        return birthYear != null &&
+                issueYear != null &&
+                expirationYear != null &&
+                height != null &&
+                hairColor != null &&
+                eyeColor != null &&
+                passportID != null &&
+                countryID != null;
+    }
+
+    public boolean isValidPassport() {
+        return (isNorthPolePassport() || containsAllFields()) &&hasValidFields();
+    }
+
+    private boolean isNorthPolePassport() {
+        return birthYear != null &&
+                issueYear != null &&
+                expirationYear != null &&
+                height != null &&
+                hairColor != null &&
+                eyeColor != null &&
+                passportID != null &&
+                countryID == null;
+    }
+
+    public boolean hasValidFields() {
+        return isBirthYearValid() &&
+                isIssueYearValid() &&
+                isExpirationYearValid() &&
+                isHeightValid() &&
+                isHairColorValid() &&
+                isEyeColorValid() &&
+                isPassportIDValid();
+    }
+
+    private boolean isBirthYearValid() {
+        return isNumberBetween(birthYear, 1920, 2002);
+    }
+
+    private boolean isIssueYearValid() {
+        return isNumberBetween(issueYear, 2010, 2020);
+    }
+
+    private boolean isExpirationYearValid() {
+        return isNumberBetween(expirationYear, 2020, 2030);
+    }
+
+    private boolean isHeightValid() {
+        boolean containsUnits = height.contains("cm") ^ height.contains("in"); // Only one of them
+        if (containsUnits) {
+            if (height.contains("cm")) {
+                return isNumberBetween(height.substring(0, height.indexOf("cm")), 150, 193);
+            }
+            if (height.contains("in")) {
+                return isNumberBetween(height.substring(0, height.indexOf("in")), 59, 76);
+            }
+        }
+        return false;
+    }
+
+    private boolean isHairColorValid() {
+        return hairColor.startsWith("#") &&
+                COLOR_REGEX.matcher(hairColor.substring(1, 7)).matches() &&
+                hairColor.length() == 7;
+    }
+
+    private boolean isEyeColorValid() {
+        return ALLOWED_EYE_COLORS.contains(eyeColor);
+    }
+
+    private boolean isPassportIDValid() {
+        return passportID.length() == 9 && isNumber(passportID);
+    }
+
+    private boolean isNumberBetween(String numberString, int from, int to) {
+        try {
+            int number = Integer.parseInt(numberString);
+            return number >= from && number <= to;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private boolean isNumber(String numberString) {
+        try {
+            Integer.parseInt(numberString);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+
+}
